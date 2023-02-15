c39603b88dd6794df7277f6a822a48b600db4451
shivihvs <cs229.shivani@gmail.com>
2023-02-15 22:58:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import axios from "axios";
+
 import './App.css';
 
 function App() {
+  var result=String({step1: { carryString: '1_', sumString: '3' }});
+  const [status, setStatus] = useState(null);
+    const[data,setData] = useState({
+      fn1: 0,
+      fn2: 0
+    });
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      if (!data.username || !data.password) {
+        setStatus(false);
+      } else {
+        setStatus(true);
+      }
+      const obj ={
+        num1:data.fn1,
+        num2:data.fn2
+      }
+      console.log(obj);
+      axios.post("http://localhost:3001/stepAddition",obj).then((response)=>{
+        setStatus(true);
+        
+      console.log(response.data);
+   })
+    };
+   const handleChange =(event) =>{
+       let { name, value } = event.target;
+      setData({ ...data, [name]: value })
+   }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <label htmlFor="fn1">First Number:</label>
+     <input type="number" id="fn1" name="fn1" value={data.fn1} onChange={handleChange}/><br></br>
+     <label htmlFor="fn2">Second Number:</label>
+     <input type="number" id="fn2" name="fn2" value={data.fn2} onChange={handleChange}/><br></br>
+     <button type="submit" onClick={handleSubmit}>Generate steps</button>
+    
     </div>
+    
   );
 }
 
 export default App;
+
+
